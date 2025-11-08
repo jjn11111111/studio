9b85181d8dd28a6a69ca26b3a4dd3d2cde4c1c30
jjn11111111 <100871704+jjn11111111@users.noreply.github.com>
2025-11-07 23:46:25 -0500

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'PinealVision',
+  description: '3rd Eye CrossTraining',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
